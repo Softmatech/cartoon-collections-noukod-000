f419112ae8cd838daebfa31f28b88aab0191c009
Andy Feidje Joseph <ingandy75@gmail.com>
2019-04-17 16:56:54 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,9 +1,8 @@
 def roll_call_dwarves(array)# code an argument here
-  # Your code here
   counter = 0
   new_array = []
   array.each do |elem|
-    new_array << "#{array[0, array.length - 1].join("#{counter}")}. #{array.last}"
+    new_array << "#{array[0, array.length - 1].join("#{counter} ")}. #{array.last}"
     counter += 1
   end
   puts new_array
