7d46ab8ec6fbb603a90212c6c7adb243b90369ba
Andy Feidje Joseph <ingandy75@gmail.com>
2019-04-17 17:01:11 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,11 +1,12 @@
 def roll_call_dwarves(array)# code an argument here
   counter = 0
   new_array = []
-  while counter < array.length
-    new_array << "#{array[0, array.length - 1].join("#{counter + 1} ")}. #{array.last}"
-    counter += 1
-  end
-  puts new_array
+  # while counter < array.length
+    # new_array << 
+    puts "#{array[0, array.length - 1].join("#{counter + 1} ")}. #{array.last}"
+    # counter += 1
+  # end
+  # puts new_array
 end
 
 def summon_captain_planet# code an argument here
