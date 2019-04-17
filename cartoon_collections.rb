c04826e7edca66cdc77e9e6f5fdb293d91899279
Andy Feidje Joseph <ingandy75@gmail.com>
2019-04-17 17:53:30 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -17,7 +17,6 @@ def long_planeteer_calls(array)# code an argument here
 end
 
 def find_the_cheese(array)# code an argument here
-  # the array below is here to help
-  array.find{|i| i == cheese_types}
   cheese_types = ["cheddar", "gouda", "camembert"]
+  array.find{|i| i == cheese_types}
 end
