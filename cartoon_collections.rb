db4d25922fcdd24d7448bfddcf6da4f8c5aa4c81
Andy Feidje Joseph <ingandy75@gmail.com>
2019-04-17 17:18:10 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -4,8 +4,11 @@ def roll_call_dwarves(array)# code an argument here
     }
 end
 
-def summon_captain_planet# code an argument here
+def summon_captain_planet(array)# code an argument here
   # Your code here
+  array.each_with_index { |item, index|
+    puts "#{item}!"
+  }
 end
 
 def long_planeteer_calls# code an argument here
